1f1cd2654aa04bc1a66a2adf73d9e563034786df
valter goes <valtergoes@outlook.com>
2025-07-22 20:39:39 -0300

    feat: alteração de animação

--- frontend/src/pages/SplashScreen.js
+++ frontend/src/pages/SplashScreen.js
@@ -1,30 +1,14 @@
 
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 function SplashScreen() {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            const isAuthenticated = localStorage.getItem("user"); // ou outro método de verificação
-            if (isAuthenticated) {
-                navigate("/home");
-            } 
-            else {
-                navigate("/home");
-            }
-        }, 2000); // espera 2 segundos
-
-        return () => clearTimeout(timer);
-    }, [navigate]);
 
     return (
         <div className="flex items-center justify-center h-screen bg-white">
             <img
                 src="images/Rental-Easy-logo.png"
                 alt="App Logo"
-                className="w-40 h-40 object-contain animate-fade-in"
+                className="w-60 h-60 object-contain animate-ping"
+                style={{ animationDuration: '2.5s' }}
             />
         </div>
     );
