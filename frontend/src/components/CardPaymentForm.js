8d62f228e024700a10ffc65a00b4ce810a2ba806
valter goes <valtergoes@outlook.com>
2025-07-23 09:04:55 -0300

    feat: Ajuste de layout

--- frontend/src/components/CardPaymentForm.js
+++ frontend/src/components/CardPaymentForm.js
@@ -3,20 +3,19 @@ import { PaymentIcon } from 'react-svg-credit-card-payment-icons';
 
 const CardPaymentForm = ({ formData, updateFields}) => {
 
-
     return (
 
         <div className="mt-3 p-4 bg-gray-50 rounded-lg border">
-            <div className="font-bold text-blue-600 mb-2">Payment details</div>
+            <div className="font-bold text-blue-600 mb-2 text-lg">Payment details</div>
 
-            <div className="mb-2">
-                <label className="block font-semibold text-xs mb-2">CardNumber</label>
+            <div className="mb-3">
+                <label className="block mb-2 text-[16px]">CardNumber</label>
                 <div className="flex gap-2 justify-start items-center w-full">
                     <PaymentIcon type="generic" format="flatRounded" width={32} />
                     <input 
                         type="text" 
                         className="border rounded px-3 py-2 max-w-[380px] w-full" 
-                        placeholder="Card Number" 
+                        placeholder="XXXX-XXXX-XXXX-XXXX" 
                         aria-labelledby="card-number" 
                         value={formData.cardNumber} 
                         onChange={e => updateFields({ cardNumber: e.target.value })} 
@@ -25,8 +24,8 @@ const CardPaymentForm = ({ formData, updateFields}) => {
                 </div>
             </div>
 
-            <div className="mb-2">
-                <label className="block mb-2 font-semibold text-xs">Cardholder Name</label>
+            <div className="mb-3">
+                <label className="block mb-2 text-[16px]">Cardholder Name</label>
                 <input 
                     type="text" 
                     className="border rounded px-3 py-2 max-w-[420px] w-full" 
@@ -37,10 +36,10 @@ const CardPaymentForm = ({ formData, updateFields}) => {
                 />
             </div>
 
-            <div className="flex gap-6 mb-2 justify-start items-center">
+            <div className="flex gap-6 mb-5 justify-start items-center">
 
                 <div>
-                    <label className="block mb-2 font-semibold text-xs">Expiry Date</label>
+                    <label className="block mb-2 text-[16px]">Expiry Date</label>
                     <input 
                         type="text" 
                         className="border rounded px-3 py-2 max-w-[80px]" 
@@ -53,7 +52,7 @@ const CardPaymentForm = ({ formData, updateFields}) => {
                 </div>
 
                 <div>
-                    <label className="block mb-2 font-semibold text-xs">CVC</label>
+                    <label className="block mb-2 text-[16px]">CVC</label>
                     <input 
                         type="text" 
                         className="border rounded px-3 py-2 max-w-[80px]" 
@@ -70,12 +69,12 @@ const CardPaymentForm = ({ formData, updateFields}) => {
             </div>
 
             <div className="flex items-center gap-3 mt-2">
-                <PaymentIcon type="visa" format="logo" width={32} />
-                <PaymentIcon type="mastercard" format="logo" width={32} />
-                <PaymentIcon type="amex" format="flatRounded" width={32} />
-                <PaymentIcon type="discover" format="flatRounded" width={32} />
-                <PaymentIcon type="jcb" format="logo" width={36} />
-                <PaymentIcon type="diners" format="logo" width={36} />
+                <PaymentIcon type="visa" format="logo" width={36} />
+                <PaymentIcon type="mastercard" format="logo" width={34} />
+                <PaymentIcon type="amex" format="flatRounded" width={34} />
+                <PaymentIcon type="discover" format="flatRounded" width={34} />
+                <PaymentIcon type="jcb" format="logo" width={38} />
+                <PaymentIcon type="diners" format="logo" width={38} />
             </div>
 
             <div className="text-xs text-yellow-700 mt-2 bg-yellow-50 p-2 rounded">
