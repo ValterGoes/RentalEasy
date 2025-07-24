e444a4194dd1c81e57fa12191c394c1be09627ad
valter goes <valtergoes@outlook.com>
2025-07-24 09:01:39 -0300

    feat: animação SplashScreen

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'logo-grow-fade': {
+          '0%': { transform: 'scale(0.5)', opacity: '0' },  
+          '100%': { transform: 'scale(1)', opacity: '1' }, 
+        }
+      },
+      animation: {
+        'logo-grow-fade': 'logo-grow-fade 1.5s ease-out forwards',
+      }
+    },
   },
   plugins: [],
 }
